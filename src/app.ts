b22714afb8cf21e5993197af99c1308efd80793b
Dammy Ola <dammyolapaix@gmail.com>
2023-01-01 12:49:01 +0000

    Started server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,47 @@
-console.log('Hello')
+/**
+ * Required External Modules
+ */
+import * as dotenv from 'dotenv'
+import express, { Express } from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+
+dotenv.config()
+
+const PORT: number = parseInt(process.env.PORT as string, 10)
+/**
+ * App Variables
+ */
+if (!process.env.PORT) {
+  process.exit(1)
+}
+
+const app: Express = express()
+
+/**
+ *  App Configuration
+ */
+app.use(helmet())
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL as string,
+    credentials: true as boolean,
+    optionsSuccessStatus: 200 as number,
+  })
+)
+app.use(express.json())
+
+/**
+ * Server Activation
+ */
+const startApp = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`App Listening on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startApp()
